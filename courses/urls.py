a423f076d7ec578cd3887696cc6e00b0407b2ebc
Shohjahon <orinboyevshohjahon16@gmail.com>
2026-09-19 23:38:25 +0500

    kurslar

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from courses.views.room import list_rooms, create_room,room_update,room_delete
+from courses.views.course import list_courses,create_course,courses_update,courses_delete
 
 
 urlpatterns=[
@@ -8,4 +9,11 @@ urlpatterns=[
     path('create/',create_room,name='create-room'),
     path('update/<int:pk>/', room_update, name='update-room'),
     path('delete/<int:pk>/', room_delete, name='delete-room'),
+
+    # courses Crud
+    path('courses/list/',list_courses,name='list-courses'),
+    path('courses/create/',create_course,name='create-courses'),
+    path('courses/update/<int:pk>/', courses_update, name='update-courses'),
+    path('courses/delete/<int:pk>/', courses_delete, name='delete-courses'),
+
 ]
--- courses/views/course.py
+++ courses/views/course.py
@@ -1,12 +1,13 @@
 from courses.forms.course import CourseForm
 from courses.models import Course
+from django.shortcuts import render,redirect
 
 def list_courses(request):
     courses = Course.objects.all()
     context = {
         'courses': courses
     }
-    return render(request, 'courses\list_courses.html', context)
+    return render(request, 'courses/course/list.html', context)
 
 
 def create_course(request):
@@ -14,9 +15,29 @@ def create_course(request):
         form = CourseForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('list_courses')
+            return redirect('list-courses')
 
     else:
         form=CourseForm()
-    return render(request, 'courses\create_course.html', {'form': form})
+    return render(request, 'courses/course/create.html', {'form': form})
+
+def courses_update(request,pk):
+    courses = Course.objects.get(pk=pk)
+    if request.method == 'POST':
+        form=CourseForm(request.POST,instance=courses)
+        if form.is_valid():
+            form.save()
+            return redirect('list-courses')
+    else:
+        form = CourseForm(instance=courses)
+    return render(request, 'courses/course/update.html', {'form': form, 'courses': courses})
+
+
+def courses_delete(request,pk):
+    courses = Course.objects.get(pk=pk)
+    if request.method == 'POST':
+        courses.delete()
+        return redirect('list-courses')
+    else:
+        return render(request, 'courses/course/delete.html', {'courses': courses})
 
